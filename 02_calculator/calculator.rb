617124b54a16a456ad3ea311a79f444680293918
sheilahd <sheila.hdez@gmail.com>
2019-12-14 18:50:16 +0000

    pig in progress

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -6,19 +6,6 @@ def subtract(a,b)
 end
 
 def sum(array)
- 
-     #array[0] || 0
-    # if array.empty? 
-    #     0
-    # else 
-    #     if array.length == 5
-    #         array.reduce(:+)
-    #      elsif array.length > 1
-    #         array.first + array[1]
-    #   else
-    #         array.first
-    #     end 
-    # end
     array.reduce(:+).to_i
 end
 
