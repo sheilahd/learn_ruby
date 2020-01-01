451e16f109ce770d8346a8f6a408c4825a6f04d9
sheilahd <sheila.hdez@gmail.com>
2020-01-01 22:13:16 +0000

    adding pig latin progress

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,28 +1,44 @@
 def translate(string)
   array = string.split(" ")
   words_trans = array.map{|word| translate_word(word)}
-  words_trans.join(" ")
+  final = words_trans.join(" ")
+  # final = words_trans.join(" ")
+  # punctuation = ['.', ',', ';', '?', '!', ':']
+  # punctuation.each do |mark|
+  #   if final.include?(mark)
+  # final.delete!(mark)
+  # final << mark 
+    #   end
+   # # end  
+    if final.include? (".")
+  final.delete! (".")
+  final << "."
+  final.capitalize
+  else
+  final
+  end
 end
-
 def translate_word(word)
-alphab = ("a".."z").to_a
-vowels = ["a", "e", "i", "o", "u"]
+alphab = ("a".."z").to_a + ("A".."Z").to_a
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I","O","U"] 
 consonants = alphab - vowels
     if vowels.include?(word[0])
       "#{word}ay"
     elsif word[0..1] == "qu"
       word[2..-1] + "quay"
-    elsif word[0..2] == "squ"
+    elsif word[0..1] == "Qu"
+      word[2..-1] + "quay"
+    elsif word[0..2] == "squ" 
       word[3..-1] + "squay"
-    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
-      word[3..-1] + word[0..2] + "ay"
+    elsif word[0..2] == "Squ" 
+       word[3..-1] + "squay"
+    elsif consonants.include?(word[0]) && consonants.include?(word[1]) &&
+    consonants.include?(word[2])
+      word[3..-1] + word[0..2].downcase + "ay"
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
       word[2..-1] + word[0..1] + "ay"
     else consonants.include?(word[0])
      word[1..-1] + word[0] + "ay"
     end
 end
-p translate("apple square")
-
-
-
+p translate("apple Apple Square Queen queen, Schmidt.")
--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -68,5 +68,15 @@ describe "#translate" do
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
+  
+  it "Keeps capitalized" do
+    s = translate("The quick brown fox.")
+    expect(s).to eq("Ethay ickquay ownbray oxfay.")
+  end
+  
+  it "Keeps punctuation marks" do
+    s = translate("the quick, brown fox")
+    expect(s).to eq("ethay ickquay, ownbray oxfay")
+  end
 
 end
